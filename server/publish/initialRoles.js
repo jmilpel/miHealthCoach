0f05e896e411d88a0d1a224199bdc2b594ae690f
jmilpel <jmilpel@gmail.com>
2018-05-25 12:23:38 +0200

    Cambio en el Navbar

--- server/publish/initialRoles.js
+++ server/publish/initialRoles.js
@@ -2,8 +2,8 @@ Meteor.startup(function () {
 	if(Meteor.users.find() < 1){
 		var id = Accounts.createUser({
 			email: 'admin@admin.com',
-			password: 'adminadmin',
-			profile: {name: 'Administrador'}
+			password: 'asdfasdf',
+			profile: {name: 'Administrator'}
 		});
 				
 		Roles.addUsersToRoles(id,'admin');
