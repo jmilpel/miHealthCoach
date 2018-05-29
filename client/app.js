3755ec9f6a812bfcc9f6b4c5a37f50ae6a66bd14
jmilpel <jmilpel@gmail.com>
2018-05-29 17:08:17 +0200

    Home page + style

--- client/app.js
+++ client/app.js
@@ -1,6 +1,34 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor'
+import { GoogleMaps } from 'meteor/dburles:google-maps';
 
 Meteor.subscribe('allPosts');
 Meteor.subscribe('comments');
+
+GoogleMaps.load();
+
+//Definición de los helpers
+Template.Footer.helpers({
+    healthMapOptions: function() {
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(19.436768, -99.187202),
+          zoom: 16
+        };
+      }
+    }
+  });
+
+Template.Footer.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('healthMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
+  });
